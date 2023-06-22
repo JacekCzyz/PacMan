af6c6eee8849e960ae400a486efb48ad00ffed0b
MJellaczyc <126816244+MJellaczyc@users.noreply.github.com>
2023-06-22 14:01:10 +0200

    synchronizacji brak, duszki chodza wszystkie, ale po scianach

--- src/pacman/Ghosts.java
+++ src/pacman/Ghosts.java
@@ -11,8 +11,8 @@ import java.util.concurrent.locks.ReentrantLock;
 import javax.swing.*;
 
 public class Ghosts extends JPanel {
-    private final int MAXN_GHOSTS = 6;
-    private int N_GHOSTS = 6;
+    private final int MAXN_GHOSTS = 4;
+    private int N_GHOSTS = 4;
     private int [] ghost_x, ghost_y, ghost_dx, ghost_dy, ghost_vel;
     private Image ghost_img;
 
@@ -22,7 +22,7 @@ public class Ghosts extends JPanel {
     public final int N_TILES = 15;
     private Thread[] ghostThreads;
     private PacGuy pac_person = new PacGuy();
-
+    private final Object[] ghostLocks;
 
 
     public Ghosts(){
@@ -34,9 +34,12 @@ public class Ghosts extends JPanel {
         dx=new int[4];
         dy=new int[4];
         ghostThreads = new Thread[N_GHOSTS];
+        ghostLocks = new Object[N_GHOSTS]; // Inicjalizacja tablicy blokad dla duchów
+        for (int i = 0; i < N_GHOSTS; i++) {
+            ghostLocks[i] = new Object(); // Inicjalizacja blokady dla konkretnego ducha
+        }
     }
 
-    private Lock lock = new ReentrantLock();
 
 
     public void ghost_set_on_level(int dx, int random, int cur_speed, int[] valid_vels){
@@ -68,27 +71,37 @@ public class Ghosts extends JPanel {
             ghostThreads[current] = new Thread(() -> move_ghosts(current, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running));
             ghostThreads[current].start();
         }*/
+/*
+        for (int i = 0; i < N_GHOSTS; i++) {
+            final int ghostIndex = i;
+            ghostThreads[ghostIndex] = new Thread(() -> move_ghosts(ghostIndex, g2d, screen_data, TILE_SIZE, N_TILES, pacman, Running));
+            ghostThreads[ghostIndex].start();
+        }*/
+
         ghostThreads[0] = new Thread(() -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
-            move_ghosts(0, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
+            move_ghosts1(0, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
         ghostThreads[0].start();
         ghostThreads[1] = new Thread(() -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
-            move_ghosts(1, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
+            move_ghosts2(1, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
         ghostThreads[1].start();
         ghostThreads[2] = new Thread(() -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
-            move_ghosts(2, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
+            move_ghosts3(2, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
         ghostThreads[2].start();
         ghostThreads[3] = new Thread(() -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
-            move_ghosts(3, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
+            move_ghosts4(3, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
         ghostThreads[3].start();
-        ghostThreads[4] = new Thread(() -> {
+
+
+
+/*        ghostThreads[4] = new Thread(() -> {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
             move_ghosts(4, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
@@ -97,104 +110,169 @@ public class Ghosts extends JPanel {
             System.out.println("Thread ID: " + Thread.currentThread().getId());
             move_ghosts(5, g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
         });
-        ghostThreads[5].start();
+        ghostThreads[5].start();*/
     }
 
-//    public void move_ghosts(int ghostIndex, Graphics2D g2d, short[] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
-//        int pacman_x = pacman.get_x();
-//        int pacman_y = pacman.get_y();
-//        boolean Pac_alive = pacman.Pac_alive;
-//        boolean hitWall = false; // Flaga oznaczająca zderzenie z ścianą
-//        move_ghosts(g2d, screen_data, TILE_SIZE, N_TILES, pacman ,Running);
-//    }
 
+//to jest najlepsze co mamy
+public void move_ghosts1(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
+    int pacman_x = pacman.get_x();
+    int pacman_y = pacman.get_y();
+    boolean Pac_alive = pacman.Pac_alive;
 
+    while (true) {
+        //synchronized (ghostLocks[ghostIndex]) {
+            if (ghost_x[ghostIndex] % TILE_SIZE == 0 && ghost_y[ghostIndex] % TILE_SIZE == 0) {
+                int pos = ghost_x[ghostIndex] / TILE_SIZE + N_TILES * (int) (ghost_y[ghostIndex] / TILE_SIZE);
+                int count = 0;
 
-    /*public void move_ghosts2(Graphics2D g2d, short[] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
-        int pos;
-        int count;
-
-        int pacman_x = pacman.get_x();
-        int pacman_y = pacman.get_y();
-        boolean Pac_alive = pacman.Pac_alive;
-        for (int i = 0; i < N_GHOSTS; i++) {
-            if (ghost_x[i] % TILE_SIZE == 0 && ghost_y[i] % TILE_SIZE == 0) {
-                pos = ghost_x[i] / TILE_SIZE + N_TILES * (int) (ghost_y[i] / TILE_SIZE);
-
-                count = 0;
-
-                if ((screen_data[pos] & 1) == 0 && ghost_dx[i] != 1) {
+                if ((screen_data[pos] & 1) == 0 && ghost_dx[ghostIndex] != 1) {
                     dx[count] = -1;
                     dy[count] = 0;
                     count++;
                 }
 
-                if ((screen_data[pos] & 2) == 0 && ghost_dy[i] != 1) {
+                if ((screen_data[pos] & 2) == 0 && ghost_dy[ghostIndex] != 1) {
                     dx[count] = 0;
                     dy[count] = -1;
                     count++;
                 }
 
-                if ((screen_data[pos] & 4) == 0 && ghost_dx[i] != -1) {
+                if ((screen_data[pos] & 4) == 0 && ghost_dx[ghostIndex] != -1) {
                     dx[count] = 1;
                     dy[count] = 0;
                     count++;
                 }
 
-                if ((screen_data[pos] & 8) == 0 && ghost_dy[i] != -1) {
+                if ((screen_data[pos] & 8) == 0 && ghost_dy[ghostIndex] != -1) {
                     dx[count] = 0;
                     dy[count] = 1;
                     count++;
                 }
 
-                if (count == 0) {
-
-                    if ((screen_data[pos] & 15) == 15) {
-                        ghost_dx[i] = 0;
-                        ghost_dy[i] = 0;
+                //synchronized (this) {
+                    if (count == 0) {
+                        if ((screen_data[pos] & 15) == 15) {
+                            ghost_dx[ghostIndex] = 0;
+                            ghost_dy[ghostIndex] = 0;
+                        } else {
+                            ghost_dx[ghostIndex] = -ghost_dx[ghostIndex];
+                            ghost_dy[ghostIndex] = -ghost_dy[ghostIndex];
+                        }
                     } else {
-                        ghost_dx[i] = -ghost_dx[i];
-                        ghost_dy[i] = -ghost_dy[i];
+                        count = (int) (Math.random() * count);
+                        if (count > 3) {
+                            count = 3;
+                        }
+                        ghost_dx[ghostIndex] = dx[count];
+                        ghost_dy[ghostIndex] = dy[count];
                     }
-
-                } else {
-
-                    count = (int) (Math.random() * count);
-
-                    if (count > 3) {
-                        count = 3;
-                    }
-
-                    ghost_dx[i] = dx[count];
-                    ghost_dy[i] = dy[count];
                 }
 
-            }
 
-            ghost_x[i] = ghost_x[i] + (ghost_dx[i] * ghost_vel[i]);
-            ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghost_vel[i]);
-            drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1);
+            ghost_x[ghostIndex] = ghost_x[ghostIndex] + (ghost_dx[ghostIndex] * ghost_vel[ghostIndex]);
+            ghost_y[ghostIndex] = ghost_y[ghostIndex] + (ghost_dy[ghostIndex] * ghost_vel[ghostIndex]);
 
-            if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
-                    && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
+            drawGhosts(g2d);
+
+            //drawGhost(g2d, ghost_x[ghostIndex] + 1, ghost_y[ghostIndex] + 1);
+            if (pacman_x > (ghost_x[ghostIndex] - 12) && pacman_x < (ghost_x[ghostIndex] + 12)
+                    && pacman_y > (ghost_y[ghostIndex] - 12) && pacman_y < (ghost_y[ghostIndex] + 12)
                     && Running) {
-
                 pacman.set_alive(false);
             }
-        }
 
-    }*/
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+       // }
+    }
+}
+
+    public void move_ghosts2(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
+        int pacman_x = pacman.get_x();
+        int pacman_y = pacman.get_y();
+        boolean Pac_alive = pacman.Pac_alive;
+
+        while (true) {
+            //synchronized (ghostLocks[ghostIndex]) {
+                if (ghost_x[ghostIndex] % TILE_SIZE == 0 && ghost_y[ghostIndex] % TILE_SIZE == 0) {
+                    int pos = ghost_x[ghostIndex] / TILE_SIZE + N_TILES * (int) (ghost_y[ghostIndex] / TILE_SIZE);
+                    int count = 0;
+
+                    if ((screen_data[pos] & 1) == 0 && ghost_dx[ghostIndex] != 1) {
+                        dx[count] = -1;
+                        dy[count] = 0;
+                        count++;
+                    }
+
+                    if ((screen_data[pos] & 2) == 0 && ghost_dy[ghostIndex] != 1) {
+                        dx[count] = 0;
+                        dy[count] = -1;
+                        count++;
+                    }
+
+                    if ((screen_data[pos] & 4) == 0 && ghost_dx[ghostIndex] != -1) {
+                        dx[count] = 1;
+                        dy[count] = 0;
+                        count++;
+                    }
+
+                    if ((screen_data[pos] & 8) == 0 && ghost_dy[ghostIndex] != -1) {
+                        dx[count] = 0;
+                        dy[count] = 1;
+                        count++;
+                    }
+
+                    if (count == 0) {
+                        if ((screen_data[pos] & 15) == 15) {
+                            ghost_dx[ghostIndex] = 0;
+                            ghost_dy[ghostIndex] = 0;
+                        } else {
+                            ghost_dx[ghostIndex] = -ghost_dx[ghostIndex];
+                            ghost_dy[ghostIndex] = -ghost_dy[ghostIndex];
+                        }
+                    } else {
+                        count = (int) (Math.random() * count);
+                        if (count > 3) {
+                            count = 3;
+                        }
+                        ghost_dx[ghostIndex] = dx[count];
+                        ghost_dy[ghostIndex] = dy[count];
+                    }
+                }
 
 
+                ghost_x[ghostIndex] = ghost_x[ghostIndex] + (ghost_dx[ghostIndex] * ghost_vel[ghostIndex]);
+                ghost_y[ghostIndex] = ghost_y[ghostIndex] + (ghost_dy[ghostIndex] * ghost_vel[ghostIndex]);
 
+            drawGhosts(g2d);
 
-//to jest najlepsze co mamy
-public void move_ghosts(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
-    int pacman_x = pacman.get_x();
-    int pacman_y = pacman.get_y();
-    boolean Pac_alive = pacman.Pac_alive;
-    while (true) {
-        synchronized (this) {
+            //drawGhost(g2d, ghost_x[ghostIndex] + 1, ghost_y[ghostIndex] + 1);
+            if (pacman_x > (ghost_x[ghostIndex] - 12) && pacman_x < (ghost_x[ghostIndex] + 12)
+                    && pacman_y > (ghost_y[ghostIndex] - 12) && pacman_y < (ghost_y[ghostIndex] + 12)
+                    && Running) {
+                pacman.set_alive(false);
+            }
+
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+             }
+       //}
+    }
+
+        public void move_ghosts3(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
+        int pacman_x = pacman.get_x();
+        int pacman_y = pacman.get_y();
+        boolean Pac_alive = pacman.Pac_alive;
+
+        while (true) {
+            //synchronized (this) {
             if (ghost_x[ghostIndex] % TILE_SIZE == 0 && ghost_y[ghostIndex] % TILE_SIZE == 0) {
                 int pos = ghost_x[ghostIndex] / TILE_SIZE + N_TILES * (int) (ghost_y[ghostIndex] / TILE_SIZE);
                 int count = 0;
@@ -247,26 +325,28 @@ public void move_ghosts(int ghostIndex, Graphics2D g2d, short [] screen_data, in
             drawGhosts(g2d);
 
             //drawGhost(g2d, ghost_x[ghostIndex] + 1, ghost_y[ghostIndex] + 1);
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
             if (pacman_x > (ghost_x[ghostIndex] - 12) && pacman_x < (ghost_x[ghostIndex] + 12)
                     && pacman_y > (ghost_y[ghostIndex] - 12) && pacman_y < (ghost_y[ghostIndex] + 12)
                     && Running) {
-
                 pacman.set_alive(false);
             }
+
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // }
         }
     }
-}
 
+    public void move_ghosts4(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, PacGuy pacman, boolean Running) {
+        int pacman_x = pacman.get_x();
+        int pacman_y = pacman.get_y();
+        boolean Pac_alive = pacman.Pac_alive;
 
-    /*public boolean move_ghosts(int ghostIndex, Graphics2D g2d, short [] screen_data, int TILE_SIZE, int N_TILES, int pacman_x, int pacman_y, boolean Running, boolean Pac_alive) {
         while (true) {
+            //synchronized (this) {
             if (ghost_x[ghostIndex] % TILE_SIZE == 0 && ghost_y[ghostIndex] % TILE_SIZE == 0) {
                 int pos = ghost_x[ghostIndex] / TILE_SIZE + N_TILES * (int) (ghost_y[ghostIndex] / TILE_SIZE);
                 int count = 0;
@@ -316,26 +396,25 @@ public void move_ghosts(int ghostIndex, Graphics2D g2d, short [] screen_data, in
             ghost_x[ghostIndex] = ghost_x[ghostIndex] + (ghost_dx[ghostIndex] * ghost_vel[ghostIndex]);
             ghost_y[ghostIndex] = ghost_y[ghostIndex] + (ghost_dy[ghostIndex] * ghost_vel[ghostIndex]);
 
-            drawGhost(g2d, ghost_x[ghostIndex] + 1, ghost_y[ghostIndex] + 1);
+            drawGhosts(g2d);
 
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+            //drawGhost(g2d, ghost_x[ghostIndex] + 1, ghost_y[ghostIndex] + 1);
             if (pacman_x > (ghost_x[ghostIndex] - 12) && pacman_x < (ghost_x[ghostIndex] + 12)
                     && pacman_y > (ghost_y[ghostIndex] - 12) && pacman_y < (ghost_y[ghostIndex] + 12)
                     && Running) {
-
-                Pac_alive = false;
+                pacman.set_alive(false);
             }
+
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // }
         }
-        return Pac_alive;
-    }*/
+    }
 
 
-   // public void drawGhost(Graphics2D g2d, int x, int y){
    public void drawGhosts(Graphics2D g2d) {
        //lock.lock();
        try {
@@ -345,4 +424,9 @@ public void move_ghosts(int ghostIndex, Graphics2D g2d, short [] screen_data, in
            //lock.unlock();
        }
    }
+
+/*    public void drawGhost(Graphics2D g2d, int x, int y){
+        g2d.drawImage(this.ghost_img, x, y, this);
+
+    }*/
 }
